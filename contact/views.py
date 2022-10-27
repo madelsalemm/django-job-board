d59025d895aa8df7f42f95ebdf8441709b53c6b7
madelsalemm <Mohamed.adel.salemm@gmail.com>
2022-10-28 01:26:18 +0200

    contact send email rev 00

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,19 @@
 from multiprocessing import context
+from django.conf import settings
 from django.shortcuts import redirect, render
 from django.urls import is_valid_path
-#from .models import job , Apply
+from .models import Info
+from django.core.mail import send_mail
 #from .form import ApplyForm , JobForm
 
+
 def send_message(request):
-    pass
+    myinfo = Info.objects.first()
+    
+    if request.method =='POST':
+        subject = request.POST['subject']
+        email = request.POST['email']
+        message = request.POST['message']
+        send_mail(subject, message, settings.EMAIL_HOST_USER , [email])
+    context = {'myinfo' : myinfo}
+    return render (request , 'contact/contact.html' , context)
--- project/settings.py
+++ project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'job',
     'django_countries',
     'bootstrap5',
-    
+    'contact',
     
 ]
 
@@ -135,3 +135,12 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , "media" )
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'Mohamed.adel.salemm@gmail.com'
+EMAIL_HOST_PASSWORD = 'egrwacelswfhjswb' #past the key or password app here
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'default from email'
